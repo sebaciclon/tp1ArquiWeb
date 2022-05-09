a30733de294d22c01fe49e82e230194851dc5eb5
seba <sebaesains77@gmail.com>
2022-05-09 11:11:27 -0300

    Se hizo esqueleto del proyecto

---
+++ Entregable2/src/dao/DAOCarrera.java
@@ -0,0 +1,7 @@
+package dao;
+
+public class DAOCarrera {
+	
+	// implementa dao
+
+}
---
+++ Entregable2/src/interfaces/Dao.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+import java.util.Optional;
+
+public interface Dao<T> {
+	
+	// Optional es un tipo de dato que permite el manejo de null
+	 
+	public Optional<T> getId(int id);
+	
+	// aca va el crud
+}
